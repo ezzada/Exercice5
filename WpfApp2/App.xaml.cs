ceb6647ed31861b6a230ca68011cce76384d2a88
ezzada <2464026@cstj.qc.ca>
2025-09-22 20:50:20 -0400

    Finished: all

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -9,6 +9,18 @@ namespace WpfApp2
     /// </summary>
     public partial class App : Application
     {
+        static public Dictionary<int,Product> dicProduit = new Dictionary<int, Product>() {
+            {114, new Product(){Id = 144, Name = "Diablo 4", Price = 49.99, Stock = 8} },
+            {74, new Product(){Id = 74, Name = "Halo 3", Price = 49.99, Stock = 3} },
+            {1027, new Product(){Id = 1027, Name = "Starfield", Price = 49.99, Stock = 11} }
+        };
+
+
+        static public Dictionary<string, Product> dicProduit2 = new Dictionary<string, Product>() {
+            {"Diablo 4", new Product(){Id = 144, Name = "Diablo 4", Price = 49.99, Stock = 8} },
+            {"Halo 3", new Product(){Id = 74, Name = "Halo 3", Price = 49.99, Stock = 3} },
+            {"Starfield", new Product(){Id = 1027, Name = "Starfield", Price = 49.99, Stock = 11} }
+        };
     }
 
 }
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -19,6 +19,88 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Product produit = new Product();
+            int id;
+            if (int.TryParse(txbID.Text, out id))
+            {
+                if (App.dicProduit.TryGetValue(id, out produit))
+                {
+                    MessageBox.Show(produit.ToString());
+                }
+                else
+                {
+                    MessageBox.Show($"Product {txbID.Text} does not exist!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Not a valide ID");
+            }
+
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Pas de produit valid pour l'id 127
+            App.dicProduit[114].Price = 59.99;
+            App.dicProduit[74].Name = "Halo 4";
+            App.dicProduit[1027].Stock = 0;
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            string ids = string.Join(", ", App.dicProduit.Keys);
+            MessageBox.Show($"Inventory contains {App.dicProduit.Count} diffente product with following ids {ids}");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var kvp in App.dicProduit)
+            {
+                Product p = kvp.Value;
+                sb.AppendLine($"{p.Id} - {p.Stock}");
+            }
+
+            MessageBox.Show(sb.ToString(), "Inventory");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            App.dicProduit.Add(2111, new Product() { Id = 2111, Name = "Baldur's Gate 3", Price = 79.99, Stock = 25 });
+            App.dicProduit2.Add("Baldur's Gate 3", new Product() { Id = 2111, Name = "Baldur's Gate 3", Price = 79.99, Stock = 25 });
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            App.dicProduit.Remove(App.dicProduit2["Starfield"].Id);
+            App.dicProduit2.Remove("Starfield");
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var kvp in App.dicProduit2)
+            {
+                if(kvp.Key == "Starfield" || kvp.Key == "Diablo 4")
+                {
+                    Product p = kvp.Value;
+                    sb.AppendLine(p.ToString());
+                }
+            }
+
+
+            MessageBox.Show(sb.ToString(), "Info sur les produit");
         }
     }
 }
